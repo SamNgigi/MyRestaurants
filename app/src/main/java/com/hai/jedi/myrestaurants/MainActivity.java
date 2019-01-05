db5947d27730a60b93a430c6a6160edfc627ecf9
SamNgigi <sam.t.ngigi@gmail.com>
2019-01-05 13:51:46 +0300

    Refactored for activity.xml to match lms.

--- app/src/main/java/com/hai/jedi/myrestaurants/MainActivity.java
+++ app/src/main/java/com/hai/jedi/myrestaurants/MainActivity.java
@@ -2,9 +2,12 @@ package com.hai.jedi.myrestaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFindRestaurantsButton;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -13,12 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Run default behaviours for an activity.
         super.onCreate(savedInstanceState);
+        // Method below tells activity which layout to use for main screen.
+        // R is short for resources. In this case we are using activity_main.xml
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+
     }
 
     /**
