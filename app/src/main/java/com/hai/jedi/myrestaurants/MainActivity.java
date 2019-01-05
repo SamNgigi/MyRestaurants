ba2216a40993bf70b2a8f0a652476a017191fdd5
SamNgigi <sam.t.ngigi@gmail.com>
2019-01-05 14:59:58 +0300

    Disabled the default text hello from c++. Was a native method implementation being called by the native library. About to create a branch for intent feature

--- app/src/main/java/com/hai/jedi/myrestaurants/MainActivity.java
+++ app/src/main/java/com/hai/jedi/myrestaurants/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hai.jedi.myrestaurants;
 
+import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,6 +10,22 @@ import android.widget.Toast;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * An activity is an application component that provides a screen with which users can interact
+     * with in order to do something (dial a phone, take photo, view map, send email). Each activity
+     * is given a window/screen in which to draw its users interface.
+     *
+     * In other words an activity can be considered as a a screen of an Android app's UI. In that
+     * ways it is very similar to a window in a desktop application.
+     *
+     * An Android app can have one or more activities meaning one or more screens.
+     *
+     * The MainActivity is thus the first screen to appear when an app is launched. Within the main
+     * activity other activities can be launched.
+     *
+     * All other activities terminate in a cascade from within the main activity.
+     * */
 
     private Button mFindRestaurantsButton;
 
@@ -25,11 +43,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        /**
+         * This method below was responsible for displaying the text "Hello from C++"
+         *
+         * TextView tv = (TextView) findViewById(R.id.sample_text);
+         *
+         * At the very bottom of file you can find where the stringFromJNI() is defined
+         *
+         * tv.setText(stringFromJNI());
+         * */
 
         /**
          * Assigning our button in xml to our var mFindRestaurantButton.
+         * 
          * We do this by getting the id we assigned to our Button.
          * findViewById returns a generic View type, so we have to type cast it because our var is
          * of type Button*/
@@ -53,5 +79,5 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+   /* public native String stringFromJNI();*/
 }
---
+++ app/src/main/java/com/hai/jedi/myrestaurants/RestaurantsActivity.java
@@ -0,0 +1,17 @@
+package com.hai.jedi.myrestaurants;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
