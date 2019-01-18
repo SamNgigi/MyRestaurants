97ea2ab910f9e7a8ad9becacb69d59f85a0ae688
SamNgigi <sam.t.ngigi@gmail.com>
2019-01-18 12:24:48 +0300

    Added a custom RestaurantListAdapter to work with the recycler view.

--- app/src/main/java/com/hai/jedi/myrestaurants/Adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/hai/jedi/myrestaurants/Adapters/RestaurantListAdapter.java
@@ -19,8 +19,34 @@ import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * Remember that an Adapter basically links a list/array of data with the View(ListView)
+ *
+ * Here we are defining a custom Adapter "RestaurantListAdapter", that extends from the RecyclerView.
+ * Adapter in order to work with more complex data such as an ArrayList of Objects.
+ *
+ * The RecyclerView.Adapter, much like the built-in Android ArrayAdapter, will populate the data into
+ * the RecyclerView.
+ *
+ * It also converts a Java object into an individual list item View to be inserted and displayed to
+ * the use.
+ *
+ * The RecyclerView.Adapter requires a ViewHolder. A ViewHolder is an object that stores multiple Views
+ * inside a Layout so they can be loaded immediately and we don't need to find them by ids repeatedly.
+ * This improves the performance
+ *
+ * The RecyclerView.Adapter requires 3 primary methods
+ * 1. onCreateViewHolder - inflating an xml layout and returns a ViewHolder
+ * 2. onBindViewHolder - sets the various information in the list item View through the ViewHolder.
+ *                        This is the moment when the data from our models get associated, aka "bound"
+ *                        to our view
+ * 3. getItemCount() - simply returns the number of items the RecyclerView will be responsible for
+ *                     listing, so that it know how many items views it will need to recycle*/
 public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+    // To calculate the item count which will inform the RecyclerView how many individual item Views
+    // it will need to recycle.
     private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+    // For our ViewHolder
     private Context mContent;
 
     public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
@@ -28,6 +54,8 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         mRestaurants = restaurants;
     }
 
+    // Below method inflates an xml layout and returns a ViewHolder. I think it populates/inflates
+    // each view it with data from the restaurant object
     @Override
     public RestaurantListAdapter.RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int ViewType){
         View view = LayoutInflater.from(parent.getContext())
@@ -38,7 +66,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
     @Override
     public void onBindViewHolder(RestaurantListAdapter.RestaurantViewHolder holder, int position){
-
+        holder.bindRestaurant(mRestaurants.get(position));
     }
 
     @Override
@@ -46,23 +74,28 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         return mRestaurants.size();
     }
 
+    // We define our ViewHolderClass that our RecycleView will need. This is where we define the data
+    // to be displayed
     public class RestaurantViewHolder extends RecyclerView.ViewHolder{
+        // We use ButterKnife to bind all views in the layout
         @BindView( R.id.restaurantImageView) ImageView mRestaurantImageView;
         @BindView(R.id.restaurantNameTextView) TextView mNameTextView;
         @BindView(R.id.tagTxtView) TextView mTagTextView;
         @BindView(R.id.ratingTxtView) TextView mRatingTextView;
 
         private Context mContext;
-
+        // I would say connecting our different Views into a single ViewHolder
         public RestaurantViewHolder(View itemView){
             super(itemView);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
         }
-
+        // We define the method below that will set the contents of the layouts TextView to the
+        // attributes specific to a restaurant.
         public void bindRestaurant(Restaurant restaurant){
             mNameTextView.setText(restaurant.getmName());
-            mTagTextView.setText(android.text.TextUtils.join(", ", restaurant.getmCategories()));
+            mTagTextView.setText(restaurant.getmCategories().get(0));
+            // mTagTextView.setText(android.text.TextUtils.join(", ", restaurant.getmCategories()));
             mRatingTextView.setText(String.format("Rating: %s/5", restaurant.getmRating()));
         }
     }
--- app/src/main/java/com/hai/jedi/myrestaurants/UI/RestaurantsActivity.java
+++ app/src/main/java/com/hai/jedi/myrestaurants/UI/RestaurantsActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.ListView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -17,6 +19,7 @@ import okhttp3.Response;
 
 import android.util.Log;
 
+import com.hai.jedi.myrestaurants.Adapters.RestaurantListAdapter;
 import com.hai.jedi.myrestaurants.Models.Restaurant;
 import com.hai.jedi.myrestaurants.Adapters.MyRestaurantsArrayAdapter;
 import com.hai.jedi.myrestaurants.R;
@@ -29,25 +32,8 @@ public class RestaurantsActivity extends AppCompatActivity {
     // Logging Constant for debugging
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
 
-    // Var that stores view we will display the welcome data.
-    // private TextView mLocationTextView;
-    @BindView( R.id.locationInfo) TextView mLocationTextView;
-    // private ListView mListView;
-    @BindView(R.id.listView) ListView mListView;
-
-    // String Array of dummy restaurants that we want to display
-    private String[] restaurants = new String[]{
-            "Kibandanski", "Mother's Bistro", "Life of Pie", "Screen Door", "Luc Lac",
-            "Sweet Basil", "Fudge Cakes", "Equinox", "Miss Delta's", "Bae's Kitchen",
-            "Edo", "Nai City Grill", "Fat Head's Brewery", "Chipotle", "Polo"
-    };
-
-    // String Array of Cuisines
-    private String[] cuisines = new String[] {
-            "Vegan Food", "Breakfast", "Fish Dish", "Scandinavian", "Coffee", "English",
-            "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food",
-            "Sports Bar", "Pizza", "Pastries"
-    };
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RestaurantListAdapter mAdapter;
 
     public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
@@ -65,8 +51,6 @@ public class RestaurantsActivity extends AppCompatActivity {
     // We retrieve the location info using our key "location_data"
         String location = restaurant_activity_intent.getStringExtra("location_data");
 
-    // Displaying location info onto screen.
-        mLocationTextView.setText("Here are all the restaurants near: " + location + ".");
         getRestaurants(location);
     }
 
@@ -83,45 +67,20 @@ public class RestaurantsActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 mRestaurants = yelpService.processResults(response);
-                Log.d(TAG, mLocationTextView.toString());
 
                 RestaurantsActivity.this.runOnUiThread(new Runnable() {
+
                     @Override
                     public void run(){
-                        String[] restaurantNames = new String[mRestaurants.size()];
-                        for (int i = 0; i < restaurantNames.length; i++) {
-                            restaurantNames[i] = mRestaurants.get(i).getmName();
-                        }
-                        Log.d(TAG, restaurantNames.toString());
-
-                        String[] restaurantCuisines = new String[mRestaurants.size()];
-                        for(int i = 0; i < restaurantCuisines.length; i++){
-                            restaurantCuisines[i] = android.text.TextUtils.join(", ",
-                                    mRestaurants.get(i).getmCategories());
-                        }
-                        /**
-                         * Simple ArrayAdapter
-                         * ArrayAdapter adapter = new ArrayAdapter<>(RestaurantsActivity.this,
-                                android.R.layout.simple_list_item_1, restaurantNames);
-                         */
-                        // Custom ArrayAdapter
-                        MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(
-                                RestaurantsActivity.this,
-                                android.R.layout.simple_list_item_1,
-                                restaurantNames, restaurantCuisines
-                                );
-                        mListView.setAdapter(adapter);
-
-                        for (Restaurant restaurant : mRestaurants) {
-                            Log.d(TAG, "Name: " + restaurant.getmName());
-                            Log.d(TAG, "Phone: " + restaurant.getmPhone());
-                            Log.d(TAG, "Website: " + restaurant.getmWebsite());
-                            Log.d(TAG, "Image url: " + restaurant.getmImageUrl());
-                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getmRating()));
-                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ",
-                                    restaurant.getmAddresses()));
-                            Log.d(TAG, "Categories: " + restaurant.getmCategories().toString());
-                        }
+                        mAdapter = new RestaurantListAdapter(getApplicationContext(), mRestaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(
+                                RestaurantsActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        // This informs the RecyclerView that its width and height should always
+                        // the same. Otherwise as individual list item views are continually recycled,
+                        // it may attempt to rest its own size to best fit the content.
+                        mRecyclerView.setHasFixedSize(true);
                     }
                 });
             }
