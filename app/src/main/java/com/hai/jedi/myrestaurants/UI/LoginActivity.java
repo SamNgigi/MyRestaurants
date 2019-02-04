d7cc89586de31444ec24b1202317ff133b26357c
SamNgigi <sam.t.ngigi@gmail.com>
2019-02-04 08:25:22 +0300

    Added anchor tags to the @string resource for login. Added fromHtml method to enable hyperlinks

--- app/src/main/java/com/hai/jedi/myrestaurants/UI/LoginActivity.java
+++ app/src/main/java/com/hai/jedi/myrestaurants/UI/LoginActivity.java
@@ -4,13 +4,39 @@ import com.hai.jedi.myrestaurants.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.registerHere) TextView signUpHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        signUpHere.setText(Html.fromHtml(getString(R.string.register_here)));
+
+        signUpHere.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view == signUpHere){
+            Toast.makeText(
+                    LoginActivity.this,
+                    "Working!!",
+                    Toast.LENGTH_LONG)
+                .show();
+        }
     }
 }
