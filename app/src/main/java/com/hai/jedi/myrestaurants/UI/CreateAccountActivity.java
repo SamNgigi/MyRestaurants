e6bfec249452b6dd5dc875ae3bce45012a6e328d
SamNgigi <sam.t.ngigi@gmail.com>
2019-02-04 10:48:51 +0300

    Added createAuthStateListener to create account activity that allows a user to auto log into app when we navigate to it

--- app/src/main/java/com/hai/jedi/myrestaurants/UI/CreateAccountActivity.java
+++ app/src/main/java/com/hai/jedi/myrestaurants/UI/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.hai.jedi.myrestaurants.UI;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseUser;
 import com.hai.jedi.myrestaurants.R;
 
 import androidx.annotation.NonNull;
@@ -32,6 +33,8 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     public static final String TAG = CreateAccountActivity.class.getSimpleName();
 
     private FirebaseAuth mAuth;
+    // Add an mAuthListener member var
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @BindView(R.id.signUpButton) Button mCreateUserButton;
     @BindView(R.id.nameEditText) EditText nNameEditText;
@@ -54,6 +57,8 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
         mCreateUserButton.setOnClickListener(this);
         mLoginHere.setOnClickListener(this);
+
+        createAuthStateListener();
     }
 
     @Override
@@ -100,5 +105,42 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                     }
              }
          );
+    }
+
+    private void createAuthStateListener () {
+        // This listens to changes in the current AuthState.
+        // Whenever there is a change (i.e a user is authenticated or signs out)
+        // this interfaces triggers the onAuthStateChanged() method.
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            // This method returns FirebaseAuth data
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                // Using this data we can create a FirebaseUser by calling the get
+                // current user method.
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                // Check that the above is not null
+                if(user != null){
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
